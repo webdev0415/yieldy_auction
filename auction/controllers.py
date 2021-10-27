8569adb5c26d7e8620fd8df790fc2c9771b684ca
webdev0415 <igor.shevk@gmail.com>
2021-10-26 22:07:11 -0400

    create nft

--- auction/controllers.py
+++ auction/controllers.py
@@ -7,6 +7,8 @@ from auction.util import (
     getBalances,
     getAppGlobalState,
     getLastBlockTimestamp,
+    printCreatedAsset,
+    printAssetHolding
 )
 from auction.testing.setup import getAlgodClient
 from auction.testing.resources import (
@@ -14,6 +16,117 @@ from auction.testing.resources import (
     optInToAsset,
     createDummyAsset,
 )
+def createNonFungibleToken():
+    sender = request.body.sender
+    amount = request.body.amount
+  # For ease of reference, add account public and private keys to
+  # an accounts dict.
+  print("--------------------------------------------")
+  print("Creating account...")
+  accounts = {}
+  m = create_account()
+  accounts[1] = {}
+  accounts[1]['pk'] = mnemonic.to_public_key(m)
+  accounts[1]['sk'] = mnemonic.to_private_key(m)
+
+
+client = getAlgodClient()
+  print("--------------------------------------------")
+  print("Creating Asset...")
+  # CREATE ASSET
+  # Get network params for transactions before every transaction.
+  params = client.suggested_params()
+  # comment these two lines if you want to use suggested params
+  params.fee = 1000
+  params.flat_fee = True
+    
+  # JSON file
+  f = open ('aliceNFTmetadata.json', "r")
+  
+  # Reading from file
+  metadataJSON = json.loads(f.read())
+  metadataStr = json.dumps(metadataJSON)
+
+  hash = hashlib.new("sha512_256")
+  hash.update(b"arc0003/amj")
+  hash.update(metadataStr.encode("utf-8"))
+  json_metadata_hash = hash.digest()
+
+
+  # Account 1 creates an asset called latinum and
+  # sets Account 1 as the manager, reserve, freeze, and clawback address.
+  # Asset Creation transaction
+  txn = AssetConfigTxn(
+      sender=sender.address,
+      sp=params,
+      total=1,
+      default_frozen=False,
+      unit_name="ALICE001",
+      asset_name="Alice's Artwork 001",
+      manager=accounts[1]['pk'],
+      reserve=None,
+      freeze=None,
+      clawback=None,
+      strict_empty_address_check=False,
+      url="https://path/to/my/asset/details", 
+      metadata_hash=json_metadata_hash,
+      decimals=0)
+
+  # Sign with secret key of creator
+  stxn = txn.sign(sender.sk)
+
+  # Send the transaction to the network and retrieve the txid.
+  txid = client.send_transaction(stxn)
+  print("Asset Creation Transaction ID: {}".format(txid))
+
+  # Wait for the transaction to be confirmed
+  wait_for_confirmation(client,txid,4)
+
+  try:
+      # Pull account info for the creator
+      # account_info = client.account_info(accounts[1]['pk'])
+      # get asset_id from tx
+      # Get the new asset's information from the creator account
+      ptx = client.pending_transaction_info(txid)
+      asset_id = ptx["asset-index"]
+      printCreatedAsset(client, sender.address, asset_id)
+      printAssetHolding(client, sender.address, asset_id)
+  except Exception as e:
+      print(e)
+
+  print("--------------------------------------------")
+  print("You have successfully created your own Non-fungible token! For the purpose of the demo, we will now delete the asset.")
+  print("Deleting Asset...")
+
+  # Asset destroy transaction
+  txn = AssetConfigTxn(
+      sender=sender.address,
+      sp=params,
+      index=asset_id,
+      strict_empty_address_check=False
+      )
+
+  # Sign with secret key of creator
+  stxn = txn.sign(sender.sk)
+  # Send the transaction to the network and retrieve the txid.
+  txid = client.send_transaction(stxn)
+  print("Asset Destroy Transaction ID: {}".format(txid))
+  # Wait for the transaction to be confirmed
+  wait_for_confirmation(client, txid, 4)
+
+  # Asset was deleted.
+  try:
+      printAssetHolding(client, sender.address, asset_id)
+      printCreatedAsset(client, sender.address, asset_id)
+      print("Asset is deleted.")
+  except Exception as e:
+      print(e)
+  
+  print("--------------------------------------------")
+  print("Sending closeout transaction back to the testnet dispenser...")
+  closeout_account(sender.address, sender.sk, client)
+
+# utility for waiting on a transaction confirmation
 
 def auction():
     client = getAlgodClient()
